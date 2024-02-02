25455c1783f641a4718d8c275694fa1633639d42
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2024-02-02 21:36:31 +0100

    add calc game script

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.core import gameplay
+from brain_games.games import brain_calc
+
+
+def main():
+    gameplay(brain_calc)
+
+
+if __name__ == '__main__':
+    main()
