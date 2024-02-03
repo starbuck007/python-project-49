06229df404531a345cadef8288d0ffb27f729d75
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2024-02-03 14:20:17 +0100

    add annotation and comments

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,22 +1,25 @@
+# Game: Get the result of the expression
 import random
 
 
 RULE = 'What is the result of the expression?'
 
 
-def get_correct_answer(numbers, operator):
+def get_correct_answer(numbers: list[int], operator: str) -> str:
+    """Get correct answer for question"""
     if operator == '-':
-        return numbers[0] - numbers[1]
+        return str(numbers[0] - numbers[1])
     elif operator == '+':
-        return numbers[0] + numbers[1]
+        return str(numbers[0] + numbers[1])
     else:
-        return numbers[0] * numbers[1]
+        return str(numbers[0] * numbers[1])
 
 
-def get_question():
+def get_question() -> tuple[str, str]:
+    """Get question and correct answer for game"""
     nums = [random.randint(0, 10) for _ in range(2)]
     operators = ['-', '+', '*']
     operator = random.choice(operators)
     question = f'{str(nums[0])} {operator} {str(nums[1])}'
-    correct_answer = str(get_correct_answer(nums, operator))
+    correct_answer = get_correct_answer(nums, operator)
     return question, correct_answer
--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,14 +1,17 @@
+# Game: Even or Not
 from random import randint
 
 
 RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def get_correct_answer(num):
+def get_correct_answer(num: int) -> str:
+    """Get correct answer for question"""
     return 'yes' if num % 2 == 0 else 'no'
 
 
-def get_question():
+def get_question() -> tuple[str, str]:
+    """Get question and correct answer for game"""
     num = randint(1, 100)
     question = f'{str(num)}'
     correct_answer = get_correct_answer(num)
