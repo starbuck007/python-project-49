4425035365dc1a9999de6c6950801526db364d22
Regina Khairutdinova <regina.khajrutdinova@gmail.com>
2024-02-03 14:08:01 +0100

    add annotation and comments

--- brain_games/core.py
+++ brain_games/core.py
@@ -1,18 +1,22 @@
+# General logic for all brain games
 import prompt
 
 
-def welcome_user():
+def welcome_user() -> str:
+    """Greeting and get user's name"""
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     return name
 
 
-def check_answer(user_answer, correct_answer):
+def check_answer(user_answer: str, correct_answer: str) -> bool:
+    """Compare user's answer and correct answer"""
     return True if user_answer == correct_answer else False
 
 
 def gameplay(game):
+    """Core gameplay for all brain games"""
     counter = 0
     name = welcome_user()
     print(game.RULE)
